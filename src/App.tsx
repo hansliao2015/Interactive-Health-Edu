3ad4bab47147be65747d3b8e17980a4e49575d89
hansliao2015 <hansliao2015@gmail.com>
2025-11-01 02:13:33 +0800

    feat: add routing with react-router-dom and create basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { Button } from "@/components/ui/button"
+import { Link, Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/HomePage"
+import { AboutPage } from "./pages/AboutPage"
+import { NotFoundPage } from "./pages/NotFoundPage"
+
 
 export function App() {
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div>
+      <nav className="fixed top-0 left-0 h-16 w-full z-50 bg-gray-200 border-b border-gray-300">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <div className="mt-16">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </div>
   )
 }
